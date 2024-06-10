7d5c75e6fc9178a8eb2b4318ccf9af9eb65e0796
ekuaazxnd <162711916+ekuaazxnd@users.noreply.github.com>
2024-06-10 16:43:19 +0100

    Update 8.js

--- 8.js
+++ 8.js
@@ -1,14 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const scrollContainer = document.querySelector('.scroll-container');
 
-     // Change cursor to click-cursor.png on mousedown
-    body.addEventListener('mousedown', function() {
-        body.style.cursor = 'url(eyes4.png), auto';
-    });
-
-    // Revert cursor to default-cursor.png on mouseup
-    body.addEventListener('mouseup', function() {
-        body.style.cursor = 'url(eyes3.png), auto';
+    
         
     scrollContainer.addEventListener('wheel', function(event) {
         // Check if the element is scrollable
